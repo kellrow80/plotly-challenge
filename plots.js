fbfbb2c5ce06a26cccf0a5f118c2922b5a03ad02
Kelly <kellrow80@comcast.net>
2021-12-16 12:05:04 -0600

    Implemented event handler and function stubs

--- plots.js
+++ plots.js
@@ -2,6 +2,29 @@
 
 console.log("plots.js loaded");
 
+function DrawBarchart(sampleId) {
+    console.log(`DrawBarchart(${sampleId})`);
+
+}
+
+function DrawBubblechart(sampleId) {
+    console.log(`DrawBubblechart(${sampleId})`);
+}
+
+function ShowMetadata(sampleId) {
+    console.log(`ShowMetadata(${sampleId})`);
+}
+
+function optionChanged(id) {
+    console.log(`optionChanged(${id})`);
+
+    DrawBarchart(id);
+    DrawBubblechart(id);
+    ShowMetadata(id);
+
+
+}
+
 
 
 function InitDashboard()
@@ -22,7 +45,14 @@ function InitDashboard()
 
 
         });
+
+        let sampleId = sampleNames[0];
+
+        DrawBarchart(sampleId);
+        DrawBubblechart(sampleId);
+        ShowMetadata(sampleId);
     });
+
 }
 
 InitDashboard();
